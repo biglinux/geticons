59e9004181d80da5c579662c0f1401f2b05b87ef
zethra <jediben97@gmail.com>
2020-08-15 03:03:47 -0400

    Added license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+// geticons - A program to get icons in Freedesktop systems
+// Copyright (C) 2020 Benjamin Aaron Goldberg <ben@benaaron.dev>
+// 
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+// PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <https://www.gnu.org/licenses/>.
+
 use argh::FromArgs;
 use linicon::{IconPath, IconType};
 use prettytable::{cell, format::consts::FORMAT_CLEAN, row, Table};
